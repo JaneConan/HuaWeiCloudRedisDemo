751eb691693191a7facc2de09454ccbff6f67e59
JaneConan <709291115@qq.com>
2019-04-22 15:00:58 +0800

    初始化项目

---
+++ src/HuaWeiCloudRedisDemo/HuaWeiCloudRedisDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace redisdemo
+{
+    class Program
+    {
+        // redis config
+        private static ConfigurationOptions connDCS = ConfigurationOptions.Parse("114.115.145.15:6379,password=!HuaWeiCloud,connectTimeout=2000");
+        //the lock for singleton
+        private static readonly object Locker = new object();
+        //singleton
+        private static ConnectionMultiplexer redisConn;
+        //singleton
+        public static ConnectionMultiplexer getRedisConn()
+        {
+            if (redisConn == null)
+            {
+                lock (Locker)
+                {
+                    if (redisConn == null || !redisConn.IsConnected)
+                    {
+                        redisConn = ConnectionMultiplexer.Connect(connDCS);
+                    }
+                }
+            }
+            return redisConn;
+        }
+        static void Main(string[] args)
+        {
+            redisConn = getRedisConn();
+            var db = redisConn.GetDatabase();
+            //set get
+            string strKey = "Hello";
+            string strValue = "DCS for Redis!";
+            Console.WriteLine(strKey + ", " + db.StringGet(strKey));
+
+            Console.ReadLine();
+        }
+    }
+}
